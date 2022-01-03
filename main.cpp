0b8f882eb294224242548e7b4c11059a3edddcd5
fsiala257 <firas.siala257@gmail.com>
2022-01-03 12:58:56 -0500

    Test

--- main.cpp
+++ main.cpp
@@ -12,8 +12,10 @@ int main()
 {
 	Sudoku sudoku;					// Create sudoku object from Sudoku class
 	sudoku.readBoard("input.json");			// Get board from input file
+
 	std::cout << std::endl;				
 	std::cout<< "Initial board: " << std::endl;
+
 	sudoku.printBoard();				// Print board
 	sudoku.solve();					// Solve sudoku
 	sudoku.writeToFile("output.json");		// Write results to file
